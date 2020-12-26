48497e7f912485f14dcde735c9a83e786ffa8040
Шапаренко Александр Александрович <shaparenko.aa@reliab.tech>
2020-12-26 12:15:56 +0300

    fix: add main body

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,9 +1,15 @@
 package com;
 
+import com.api.GitHubApi;
+import com.api.GitHubApiImpl;
+
 public class Main {
 
     public static void main (String[] args){
+        GitHubApi api = new GitHubApiImpl();
 
+        api.getRepos("burwestnik")
+                .forEach(System.out::println);
     }
 
 }
